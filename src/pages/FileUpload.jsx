5f93dbf975ec7bd90e6b66985ec47e59b7ee22d2
arnab <arnabchatterjee.ac.2@gmail.com>
2023-12-20 16:47:43 +0530

    style - fixed the file upload style

--- src/pages/FileUpload.jsx
+++ src/pages/FileUpload.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import { useFiles } from "../hooks/useFiles";
 import { useParams } from "react-router";
+import { RiFolderUploadFill } from "react-icons/ri";
 
 function FileUpload() {
   const [file, setFile] = useState(null);
-  const { upload, isUploadLoading , addFileToBlockchain} = useFiles();
-  const {caseId} = useParams()
+  const { upload, isUploadLoading, addFileToBlockchain } = useFiles();
+  const { caseId } = useParams();
   const handleChange = (file) => {
-    console.log(file)
+    console.log(file);
     setFile(file);
   };
 
@@ -21,13 +22,30 @@ function FileUpload() {
     }
   }
   return (
-    <div>
-      <FileUploader handleChange={handleChange} name="file" />
-      {file?.name}
-      <div>
-        <button onClick={() => uploadFile()}>Upload</button>
+    <section className="bg-slate-950 min-h-screen flex justify-center items-center">
+      <div className="flex flex-col">
+        <h3 className="text-white font-bold text-4xl mb-4">
+          Add files <span className="text-blue-600">here</span>
+        </h3>
+        <div className="flex flex-row">
+          <FileUploader handleChange={handleChange} name="file">
+            <div className="flex justify-center p-10 h-96 w-60 items-center bg-gray-100">
+              <RiFolderUploadFill className="w-20 h-20 text-gray-700" />
+            </div>
+          </FileUploader>
+          <div className="flex flex-col ml-4">
+              <p className="text-white font-bold mb-2">
+                Add <span className="text-green-500">description</span> below
+              </p>
+              <textarea className="bg-black text-white h-72 w-80 p-4 rounded-xl border-2 border-white" placeholder="add metadata here"/>
+              <p className="text-white">{file?.name}</p>
+        <div className=" bg-gradient-to-r from-blue-500 via-blue-800 to-purple-600 mt-4 p-4 flex text-white justify-center items-center rounded-xl">
+          <button onClick={() => uploadFile()}>Upload Files</button>
+        </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
--- src/pages/lawyer.jsx
+++ src/pages/lawyer.jsx
@@ -56,7 +56,7 @@ const Lawyer = () => {
             </div>
             <div className="flex flex-col lg:flex-row justify-between">
               <div className="flex flex-row">
-                <button onClick={()=>nav("/upload")} className="p-3 text-xl w-60 lg:w-80 mt-4 lg:mt-10 flex items-center mr-2 bg-gradient-to-r from-pink-500 to-purple-800 hover:bg-gradient-to-r hover:from-pink-700 hover:to-purple-900 rounded-2xl justify-center focus:outline-none">
+                <button onClick={()=>nav("/upload")} className="p-3 text-xl w-60 lg:w-80 mt-4 lg:mt-10 flex items-center mr-2 bg-gradient-to-r from-yellow-300 to-pink-600 hover:bg-gradient-to-r hover:from-pink-700 hover:to-purple-900 rounded-2xl justify-center focus:outline-none">
                   Upload Files
                   <RiUpload2Fill className="ml-2" />
                 </button>
@@ -65,11 +65,11 @@ const Lawyer = () => {
                 </button>
               </div>
               <div className="flex">
-                <button className="p-3 text-xl w-80 mt-4 lg:mt-10 flex items-center mr-4 bg-gradient-to-r from-pink-500 to-purple-800 hover:bg-gradient-to-r hover:from-pink-700 hover:to-purple-900 rounded-2xl justify-center focus:outline-none">
+                <button className="p-3 text-xl w-80 mt-4 lg:mt-10 flex items-center mr-4 bg-gradient-to-r from-green-600 via-green-500 to-yellow-300 hover:bg-gradient-to-r hover:from-pink-700 hover:to-purple-900 rounded-2xl justify-center focus:outline-none">
                   Show Case ID
                   <RiEyeFill className="ml-2" />
                 </button>
-                <button className="p-3 text-xl w-80 mt-4 lg:mt-10 flex items-center mr-2 bg-gradient-to-r from-pink-500 to-purple-800 hover:bg-gradient-to-r hover:from-pink-700 hover:to-purple-900 rounded-2xl justify-center focus:outline-none">
+                <button className="p-3 text-xl w-80 mt-4 lg:mt-10 flex items-center mr-2 bg-gradient-to-r from-yellow-500 to-orange-800 hover:bg-gradient-to-r hover:from-pink-700 hover:to-purple-900 rounded-2xl justify-center focus:outline-none">
                   Share with CLIENT
                   <FaShareAlt className="ml-2" />
                 </button>
